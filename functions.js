39737be9ce6440866b67654c1a1e8288d549e3be
Monique.Bass01 <monique.bass01@gmail.com>
2020-01-20 19:05:21 -0600

    added code

--- functions.js
+++ functions.js
@@ -12,6 +12,16 @@ console.log(nuggetizer('fish'));
 
 const dogBreed=(dog) => {
     return `my favorite dog breed is ${dog}`;
+    };
     console.log(dogBreed('lab'));
     console.log(dogBreed('actually a cat'));
-}
+
+    const fortyTwo= (number) =>{
+        return number + 42;
+    };
+console.log(fortyTwo(4));// 46
+    
+const oldAge = (year) =>{
+        return 2099-year;
+    };
+    console.log(oldAge(1954));//145
