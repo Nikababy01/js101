0f155dd795acac3809d89edcd1373d5184e7e8f1
Monique.Bass01 <monique.bass01@gmail.com>
2020-01-20 20:44:13 -0600

    new starts with M challenge

--- object.js
+++ object.js
@@ -18,3 +18,43 @@ const ageCheck = (age) => {
 
 ageCheck(12); // 'no drinks for you'
 ageCheck(24); 
+
+
+const expense ={
+    dateCreated: '01/17/2020',
+    location: 'Baja Burritor',
+    cost: 12.57,
+    isGood: true
+};
+
+// dot notation
+console.log(expense.cost);
+
+// bracket notation
+console.log(expense['cost']);
+
+const employee ={
+    firstName: 'Monique',
+    lastName: 'Bass',
+    badge: 384,
+    payPeriod: 'Bi-Weekly',
+    dateOfHire: '07/10/1999',
+    eligibleRehire: true
+    
+};
+
+//challenge
+// if employee name starts with "M "create status of vip
+//else peasant
+
+const statusMaker= (employee) =>{
+if(employee.firstName[0] === 'M'){
+employee.status = 'vip';
+} else {
+    employee.status= 'peasant';
+ }
+return employee;
+}
+
+console.log(statusMaker(employee));
+
